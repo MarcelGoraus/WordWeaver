14ff40d37a0a6d47bf31fd64db7466aac9359f4c
Marcel Goraus <128155279+MarcelGoraus@users.noreply.github.com>
2024-01-25 13:01:45 +0000

    middleware UPDATE

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,28 @@
-// middleware.tsx
+// middleware.ts
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import subdomains from "./subdomains.json";
 
 export const config = {
   matcher: ["/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"],
 };
-export default async function middleware(req) {
+
+export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
   const hostname = req.headers.get("host");
 
   // Define allowed Domains (localhost and production domain)
-  const allowedDomains = ["5v3n4d-3000.csb.app", "wordweaver.com"];
+  const allowedDomains: string[] = ["localhost:3000", "wordweaver.com"];
 
   // Verify if hostname exist in allowed domains
   const isAllowedDomain = allowedDomains.some((domain) =>
-    hostname.includes(domain),
+    hostname?.includes(domain),
   );
 
   // Extract the possible subdomain in the URL
-  const subdomain = hostname.split(".")[0];
+  const subdomain = hostname?.split(".")[0];
 
-  // If we stay in a allowed domain and its not a subdomain, allow the request.
+  // If we stay in an allowed domain and it's not a subdomain, allow the request.
   if (isAllowedDomain && !subdomains.some((d) => d.subdomain === subdomain)) {
     return NextResponse.next();
   }
@@ -28,7 +30,7 @@ export default async function middleware(req) {
   const subdomainData = subdomains.find((d) => d.subdomain === subdomain);
 
   if (subdomainData) {
-    // Rewrite the URL in the dynamic route based in the subdomain
+    // Rewrite the URL in the dynamic route based on the subdomain
     return NextResponse.rewrite(
       new URL(`/${subdomain}${url.pathname}`, req.url),
     );
